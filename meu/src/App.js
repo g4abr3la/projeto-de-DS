dd5a17f2bbc4ea95cf3b232b606c5d0ea80e5007
Gabriela <gabriellaxavier1015@gmail.com>
2023-05-13 19:51:20 -0300

    first commit

---
+++ meu/src/App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+
+const name= 'Gabriela'
+  return (
+    <div className="App">
+      
+    </div>
+  );
+}
+
+export default App;
